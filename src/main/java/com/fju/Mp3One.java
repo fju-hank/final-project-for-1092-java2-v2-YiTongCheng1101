b6e1d4080b32360c52c949c2dc71a66bf21761b2
YiTongCheng1101 <75658971+YiTongCheng1101@users.noreply.github.com>
2021-06-28 15:47:12 +0800

    Add files via upload

---
+++ src/main/java/com/fju/Mp3One.java
@@ -0,0 +1,9 @@
+package com.fju.tina;
+
+public class Mp3One extends Mp3{
+    public Mp3One(){
+        price = 60;
+        minute= 30;
+        id = "m1";
+    }
+}
---
+++ src/main/java/com/fju/Mp3Three.java
@@ -0,0 +1,9 @@
+package com.fju.tina;
+
+public class Mp3Three extends Mp3{
+    public Mp3Three(){
+        price =40;
+        minute= 30;
+        id="m3";
+    }
+}
